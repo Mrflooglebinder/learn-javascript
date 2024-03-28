fa491e98158467d488ebef64cd4c81a1cf297d1f
Floogle <mrflooglebinder@gmail.com>
2024-03-28 15:07:00 +0800

    Operator - Comparison

---
+++ operator-Comparison.js
@@ -0,0 +1,19 @@
+// Comparison Operators
+
+let x =1;
+
+// Relational Operators
+console.log('Relational Operators');
+console.log(x > 0);
+console.log(x >= 1);
+console.log(x < 1);
+console.log(x <= 1);
+console.log(' ');
+
+// Equality Operator
+console.log('Equality Operator');
+// x is equal to 1
+console.log(x === 1);
+
+// x is not equal to 1
+console.log(x !== 1);
