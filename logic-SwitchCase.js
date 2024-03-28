fbfd1b8b4233e0a3e5c18f1f1c92e02f6ffab418
Floogle <mrflooglebinder@gmail.com>
2024-03-28 20:12:44 +0800

    Logical - Switch...Case Vs if...Else Added if logic to acheive the same results.

--- logic-SwitchCase.js
+++ logic-SwitchCase.js
@@ -5,13 +5,16 @@ let role;
 
 switch (role) {
     case "guest":
-        console.log("Guest User");
+        console.log("Sw...Case Guest User");
         break;
-
     case "moderator":
-        console.log("Moderator User");
+        console.log("Sw...Case Moderator User");
         break;
-
     default:
-        console.log("Unknown User");
+        console.log("Sw...Case Unknown User");
 }
+
+// same as above but using if else
+if (role === 'guest') console.log('if...else Guest User');
+else if (role === 'moderator') console.log('if...else Moderator User');
+else console.log('if...else Unknown User');
