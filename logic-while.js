a0db54cee43ac01cfc3039463f856ad67381e518
Floogle <mrflooglebinder@gmail.com>
2024-03-28 21:52:11 +0800

    While Loop

---
+++ logic-while.js
@@ -0,0 +1,25 @@
+// While loop
+
+//For loop
+// Incrementing - with if statement to only print odd numbers
+for (let i =0; i <= 5; i++) {
+    if (i % 2 !== 0) console.log(i)
+}
+console.log('')
+
+// For loop vs while loop
+// in a for loop the loop variable is defined in the loop itself
+// in a while loop the loop variable is defined outside the loop
+
+// While loop - Basic syntax
+// let i = 0;
+// while (condition) {
+//     statement
+//     i++;
+// }
+
+let i = 0;
+while (i <= 5) {
+    if (i % 2 !== 0) console.log(i)
+    i++;
+}
