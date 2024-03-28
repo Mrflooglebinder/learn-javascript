55b678894da660da93ee85d699ba178895615c20
Floogle <mrflooglebinder@gmail.com>
2024-03-28 15:15:10 +0800

    Operator - Precedence

---
+++ operator-Precedence.js
@@ -0,0 +1,9 @@
+// Operator Precedence
+
+// Operator precedence determines the order in which operators are evaluated in an expression. 
+let x = 2 + 3 * 4; // 14
+console.log(x); // 14  
+
+//You can override operator precedence by using parentheses.
+let y = (2 + 3) * 4; // 20
+console.log(y); // 20
