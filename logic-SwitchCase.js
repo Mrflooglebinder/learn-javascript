2201519e4259d17d6644992def254dd72e6cf849
Floogle <mrflooglebinder@gmail.com>
2024-03-28 20:06:17 +0800

    Logical - Switch...Case

---
+++ logic-SwitchCase.js
@@ -0,0 +1,17 @@
+
+let role;
+
+// role = "moderator";
+
+switch (role) {
+    case "guest":
+        console.log("Guest User");
+        break;
+
+    case "moderator":
+        console.log("Moderator User");
+        break;
+
+    default:
+        console.log("Unknown User");
+}
