b31724cb66cda39bd2c12c4d748712f45f5b573d
Floogle <mrflooglebinder@gmail.com>
2024-03-28 15:09:18 +0800

    Operator - Ternary

---
+++ operator-Ternary.js
@@ -0,0 +1,11 @@
+// Ternary Operator
+
+// If client has more than 100 points
+// They are a gold client
+// Otherwise
+// They are a silver client
+
+let points = 110;
+let type = points > 100 ? 'gold' : 'silver';
+
+console.log(type);
