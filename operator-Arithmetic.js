1aa6ba75a00446b9a84c89b7b936901d448320e7
Floogle <mrflooglebinder@gmail.com>
2024-03-28 15:04:45 +0800

    Operator - Arithmetic

---
+++ operator-Arithmetic.js
@@ -0,0 +1,37 @@
+// Arithmetic Operators
+
+let x = 10;
+let y = 3;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+console.log(x ** y);    // x to the power of y
+
+// Increment (++)
+console.log(x);     // prints x to console
+console.log(x++);   // prints x to console and then increments x up by 1
+console.log(x);     // prints the latest value of x which has incremented up since the last print.
+console.log(' ');
+
+console.log(x);     // prints x to console
+console.log(++x);   // increments x up by 1 and then prints x to console 
+console.log(x);     // prints the latest value of x which has not changed since the last print.
+console.log(' ');
+
+
+// Similarly this can be done with decrement
+// Decrement (--)
+x = 10;
+y = 3
+console.log(x);
+console.log(x--);
+console.log(x);
+console.log(' ');
+
+console.log(x);
+console.log(--x);
+console.log(x);
+
