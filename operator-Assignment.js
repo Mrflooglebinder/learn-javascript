c2bbffd730e611ff1e418c2e9016033002cf9fdf
Floogle <mrflooglebinder@gmail.com>
2024-03-28 15:05:55 +0800

    Operator - Assignments

---
+++ operator-Assignment.js
@@ -0,0 +1,23 @@
+// ASSIGNMENT OPERATORS - COMMENTED OUT ALL OF THE ABOVE
+
+let x = 10;
+console.log(x);
+// this.....
+x++;
+console.log(x);
+// is th same as this.
+x = x + 1;
+console.log(x);
+
+//but to increment by more than one at a time like this.
+x = x + 5;
+console.log(x);
+// You can get the same result by doing this... 
+// this uses the [addition operator] and the [assignment operator] "="
+x += 5;
+console.log(x);
+
+
+// or use the [multiplication operator] and the [assignment operator] "="
+x *= 3;
+console.log(x);
