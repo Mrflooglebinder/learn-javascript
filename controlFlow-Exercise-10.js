5fb4ea05754985bd45b4ffaaf1a45ff2b052960f
Floogle <mrflooglebinder@gmail.com>
2024-03-29 21:23:45 +0800

    Control Flow Exercise 10

---
+++ controlFlow-Exercise-10.js
@@ -0,0 +1,13 @@
+// Control Flow Exercise 10
+// Nested Loop
+
+showStars(10);
+
+function showStars(rows) {
+    for (let row = 1; row <= rows; row++){
+        let pattern = '';
+        for (let i = 0; i < row; i++)
+        pattern += '*';
+        console.log(pattern);
+    }
+}
