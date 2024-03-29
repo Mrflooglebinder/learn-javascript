a6644326f76271c194d4e617f027b3871f736063
Floogle <mrflooglebinder@gmail.com>
2024-03-29 08:49:58 +0800

    Control Flow Exercise 1 - Answer(s)

--- controlFlow-Exercise-1.js
+++ controlFlow-Exercise-1.js
@@ -1,16 +1,26 @@
 // Write a func that takes two numbers and returns the max of the two.
-let num1 = 0;
-let num2 = 0;
 
-function maxNum(num1, num2){
-    if(num1 > num2)
-        console.log(num1);
-    else if(num2 > num1)
-        console.log(num2);
-    else 
-        console.log('The numbers are equal');
+let number = max(4, 4);
+// console.log(number);
+
+// this function checks if a is greater than b, if it is it returns a, if not it returns b
+// but this is not a clean implementation of the code -  and is a bit verbose
+function max(a, b) {
+  if (a > b) return a;
+  else return b;
 }
+console.log(number);
 
-maxNum(5, 10); // result: 10
-maxNum(15, 10); // result: 15
-maxNum(156.23, 156.22); // result: 156.23
+// this is a cleaner implementation of the above code - we dont need the else statement
+function max(a, b) {
+  if (a > b) return a;
+  return b;
+}
+console.log(number);
+
+// this is a cleaner implementation of the above code - we dont need the if statement
+// we can use the ternary operator
+function max(a, b) {
+  return (a > b) ? a : b;
+}
+console.log(number);
