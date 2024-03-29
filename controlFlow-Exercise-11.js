65f97049431eddb31f07f2f90ab706fe509314db
Floogle <mrflooglebinder@gmail.com>
2024-03-29 21:58:04 +0800

    Control Flow Exercise 11 - Answer(s)

--- controlFlow-Exercise-11.js
+++ controlFlow-Exercise-11.js
@@ -8,19 +8,17 @@
 // 12 = 1, 2, 3, 4, 6, 12
 // can be divided evenly by its factors
 
-showPrimes(10);
+showPrimes(69);
 
 function showPrimes(limit) {
-    for (let number = 2; number <= limit; number++) {
-    
-        let isPrime = true;
-        for (let factor = 2; factor < number; factor++){
-            if (number % factor === 0) {
-                isPrime = false;
-                break;
-            }
-        }
+    for (let number = 2; number <= limit; number++)
+        if (isPrime(number)) console.log(number);
+}
 
-        if (isPrime) console.log(number);
-    }
+function isPrime(number) {
+    for (let factor = 2; factor < number; factor++)
+        if (number % factor === 0) 
+            return false;
+
+    return true;
 }
