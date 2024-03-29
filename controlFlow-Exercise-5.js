6279481c11db98e0d73b842ac6b1d0854915ac08
Floogle <mrflooglebinder@gmail.com>
2024-03-29 10:42:31 +0800

    Control Flow Exercise 5 - Answer(s)

--- controlFlow-Exercise-5.js
+++ controlFlow-Exercise-5.js
@@ -13,3 +13,12 @@ function showNumbers(limit) {
         console.log(i, message);
     }
 }
+
+
+// // another way to achieve the same...
+// function showNumbers(limit) {
+//     for (let i = 0; i <= limit; i++) {
+//         if (i % 2 === 0) console.log(i, 'EVEN');
+//         else console.log(i, 'ODD');
+//     }
+// }
