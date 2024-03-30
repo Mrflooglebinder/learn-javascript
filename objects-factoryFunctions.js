fa551d1c721adb7059840d79af3f2fd301e97d02
Floogle <mrflooglebinder@gmail.com>
2024-03-30 08:52:40 +0800

    Objects Factory Function - Reuse to Create Objects

--- objects-factoryFunctions.js
+++ objects-factoryFunctions.js
@@ -9,3 +9,14 @@ function createCircle(radius) {
         }
     };
 }
+
+// Create Circle Objects
+// Can continue to create more circle objects from the same factory function
+// note: the draw method is not duplicated for each object -  it is referenced from the factory function 
+// So if in future we need to change the functionality of draw method, we can do it in one place. 
+const circle1 = createCircle(1);
+console.log(circle1);
+
+// reusing the factory function to create another circle object
+const circle2 = createCircle(2);
+console.log(circle2);
