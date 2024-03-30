5396fe625968597a2e3e71eb828888e316d3a9cf
Floogle <mrflooglebinder@gmail.com>
2024-03-30 08:44:10 +0800

    Objects Factory Function Build from Object

---
+++ objects-factoryFunctions.js
@@ -0,0 +1,26 @@
+
+const circle = {
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1
+    },
+    isVisible: true,
+    draw: function() {
+        console.log('draw');
+    }
+};
+
+const circle2 = {
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1
+    },
+    isVisible: true,
+    draw: function() {
+        console.log('draw');
+    }
+};
+
+circle.draw(1);
