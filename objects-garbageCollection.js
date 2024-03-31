c995ab1f22de0322fab2f2734ca216fcc2faf40b
Floogle <mrflooglebinder@gmail.com>
2024-03-31 14:20:12 +0800

    Objects - Garbage Collection

---
+++ objects-garbageCollection.js
@@ -0,0 +1,6 @@
+// Objects: Garbage Collection
+
+// in JS we don't have to worry about memory allocation and deallocation
+// the JS engine has a garbage collector that automatically allocates memory when objects are created
+// and deallocates memory when objects are no longer used
+// you can't control the garbage collector - so we dont need to worry about memory management - Excellent!
