1603829b96eebb1bbd1427a51238ab9fe983b9e6
Floogle <mrflooglebinder@gmail.com>
2024-04-01 13:33:16 +0800

    Arrays - Sorting Arrays of Objects | Case Insensitive

--- arrays-sortingArraysOfObjects.js
+++ arrays-sortingArraysOfObjects.js
@@ -8,31 +8,23 @@ const courses = [
 
 console.log(courses);
 
+// This code block sorts the courses array by the name of the courses in ascending order
+// This is done by using the sort() method on the courses array & passing in a function as an argument
+// the function takes two parameters, a & b, which represent two objects in the courses array
+// then the function creates two variables, nameA & nameB, to store the names of the courses from the original array
+// the variables are converted to uppercase to make the comparison case-insensitive
+// then the function compares the two names in uppercase
+// if the first name is less than the second name, the function returns -1 - meaning the first name should come before the second name
+// if the first name is greater than the second name, the function returns 1 - meaning the first name should come after the second name
+// if the names are equal, the function returns 0 - meaning the order of the two names should not change
+// this iteration continues until the array is sorted in ascending order and all the comparison checks return 0 
 courses.sort(function(a, b) {
-    if (a.name < b.name) return -1;
-    if (a.name > b.name) return 1;
-    return 0;
+    // create two variables to store the names of the courses from the original array
+    const nameA = a.name.toUpperCase(); 
+    const nameB = b.name.toUpperCase();
+
+    // compare the two names
+    if (nameA < nameB) return -1; // if the first name is less than the second name, return -1
+    if (nameA > nameB) return 1; // if the first name is greater than the second name, return 1
+    return 0; // if the names are equal, return 0
     }); 
-
-// NOTE: Once sorted you might notice that the capital 'J' in 'JavaScript' comes before the Capital 'N' in 'Node.js'
-// but the lowercase 'j' in 'javaScript' comes after the capital 'N' in 'Node.js'
-// this is beause the sorting is using the numerical values of the characters in the ASCII table
-// Seen in the redacted table below the letters N, J & j in numerical order is as follows:
-// 74 = J
-// 78 = N
-// 106 = j
-
-// Dec  Char                           Dec  Char     Dec  Char     Dec  Char
-// -------------------------------------------------------------------------
-// --------------------------------Redacted---------------------------------
-//   7  BEL (bell)                      39  '         71  G        103  g
-//  10  LF  (NL line feed, new line)    42  *         74  J        106  j
-//  11  VT  (vertical tab)              43  +         75  K        107  k
-//  12  FF  (NP form feed, new page)    44  ,         76  L        108  l
-//  13  CR  (carriage return)           45  -         77  M        109  m
-//  14  SO  (shift out)                 46  .         78  N        110  n
-// --------------------------------Redacted---------------------------------
-
-
-// View the full ascii table here: https://www.asciitable.com/
-// ascii is an acronym for - American Standard Code for Information Interchange
