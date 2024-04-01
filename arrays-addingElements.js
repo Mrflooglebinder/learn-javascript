0a3b03abea651f84d3491ea0f6a339f218dc087d
Floogle <mrflooglebinder@gmail.com>
2024-04-01 09:36:45 +0800

    Arrays - Adding Elements

---
+++ arrays-addingElements.js
@@ -0,0 +1,15 @@
+// Arrays: Adding Elements
+
+const numbers = [3, 4];
+
+// add element at the end of the array
+numbers.push(5, 6);
+
+// add element at the beginning of the array
+numbers.unshift(1, 2);
+
+// add elements before a specific index
+numbers.splice(2, 0, 'a', 'b');
+
+
+console.log(numbers);
