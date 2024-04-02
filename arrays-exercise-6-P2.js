991e9044ce7af84906259eeaa47fe8ea774eb8d8
Floogle <mrflooglebinder@gmail.com>
2024-04-02 10:27:49 +0800

    Arrays - Exercise 6 | Part 2 | Reduce Method | Answer(s)

--- arrays-exercise-6-P2.js
+++ arrays-exercise-6-P2.js
@@ -2,6 +2,7 @@
 
 const numbers = [1, 2, 3, 4, 5, -5];
 // const numbers = [];
+// const numbers = [-1, -2, -3, -4, -5];
 
 
 // Create a function that takes an array of numbers and returns the maximum number.
@@ -11,8 +12,18 @@ console.log(max);
 
 function getMax(array) {
     if (array.length === 0) return undefined;
-    return array.reduce((accumulator, currentValue) => {
-        (accumulator < currentValue) ? accumulator = currentValue : accumulator; 
-        return accumulator;
-    });
+    return array.reduce((accumulator, currentValue) => 
+        (accumulator > currentValue) ? accumulator : currentValue)
+}
+
+
+// This is the lecturer's solution
+// this is way less wordy - nice & clean...
+const max2 = getMax2(numbers);
+console.log(max2);
+
+function getMax2(array) {
+    if (array.length === 0) return undefined;
+    
+    return array.reduce((a, b) => (a > b) ? a : b);
 }
