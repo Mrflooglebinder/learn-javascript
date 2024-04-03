c6e74b583b5b165fce9238e940af640f2d52b1c9
Floogle <mrflooglebinder@gmail.com>
2024-04-03 19:56:51 +0800

    Functions - Exercise 3 Error Handling

---
+++ functions-Exercise-3.js
@@ -0,0 +1,28 @@
+// Functions Exercise 3 - Error Handling
+
+try {
+// const numbers = [1, 2, 3, 1, 1, 4, 5, 1];
+// Used for testing
+const numbers = 1;
+// console.log(numbers);
+// console.log(typeof numbers);
+// console.log(Array.isArray(numbers));
+
+const count = countOccurrences2(numbers, 1);
+
+console.log(count);
+}
+
+catch(e) {
+    console.log(e);
+}
+
+function countOccurrences2(array, searchElement) {
+    if (!Array.isArray(array))
+    throw new Error('Value is not an array.');
+
+    return array.reduce((a, b) => {
+        const occurrence = (b === searchElement) ? 1 : 0;
+        return a + occurrence;
+    }, 0);
+}
