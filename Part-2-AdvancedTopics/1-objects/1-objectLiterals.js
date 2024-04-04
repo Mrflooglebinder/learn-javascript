8218d88f544a253aee6422e0fd3fd00fe5db679e
Floogle <mrflooglebinder@gmail.com>
2024-04-04 15:54:28 +0800

    Objects - Object Literals

---
+++ Part-2-AdvancedTopics/1-objects/1-objectLiterals.js
@@ -0,0 +1,17 @@
+// Objects: Object Literals
+
+const circle = {
+    // Property
+    radius: 1,
+    // Property as an object
+    location: {
+        x: 1,
+        y: 1
+    },
+    // Method
+    draw: function() {
+        console.log('draw');
+    }
+};
+
+circle.draw(); // Calling the draw method of the circle object
