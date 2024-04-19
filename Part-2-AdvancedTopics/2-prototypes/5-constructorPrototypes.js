d2d96d11df4d3a031c3f9611af9f6ed2520c890a
Floogle <mrflooglebinder@gmail.com>
2024-04-19 18:20:58 +0800

    Prototypes - Constructor Prototypes

---
+++ Part-2-AdvancedTopics/2-prototypes/5-constructorPrototypes.js
@@ -0,0 +1,34 @@
+// // Prototypes: Constructor Prototypes
+
+// Object.getPrototypeOf(myObj);
+
+// // myObj.__proto__ (parent of myObj)
+// // Constructor.prototype (parent of myObj)
+
+
+// function Circle(radius) {
+//     this.radius = radius;
+// }
+
+// Circle.prototype;
+
+// const circle = new Circle(1);
+
+
+
+
+// // Type this in the console
+// let obj = {};
+// undefined
+// and you will get this output
+
+// obj.__proto__
+// {__defineGetter__: ƒ, __defineSetter__: ƒ, hasOwnProperty: ƒ, __lookupGetter__: ƒ, __lookupSetter__: ƒ, …}
+
+
+// // Type this in the consol
+// Object.prototype
+
+// // and you will get this output
+// {__defineGetter__: ƒ, __defineSetter__: ƒ, hasOwnProperty: ƒ, __lookupGetter__: ƒ, __lookupSetter__: ƒ, …}
+
