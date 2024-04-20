56e78aae71c9f5f6dbca9035222d3178ceb5e37a
Floogle <mrflooglebinder@gmail.com>
2024-04-20 14:27:52 +0800

    Prototypical Inheritance - Resetting the Constructor

---
+++ Part-2-AdvancedTopics/3-prototypicalInheritance/2-resettingTheConstructor.js
@@ -0,0 +1,33 @@
+// Prototypical Inheritance: Resetting the Constructor
+
+
+function Shape() {
+
+}
+
+Shape.prototype.duplicate = function() {
+    console.log('duplicate');
+};
+
+Shape.prototype.draw = function() {
+    console.log('draw');
+};
+
+function Circle(radius) {
+    this.radius = radius;
+}
+
+Circle.prototype = Object.create(Shape.prototype);
+
+// Reset the constructor
+Circle.prototype.constructor = Circle;
+
+
+function Square(size) {
+    this.size = size;
+}
+Square.prototype = Object.create(Shape.prototype);
+
+const sh = new Shape();
+const c = new Circle(1);
+const s = new Square(1);
