18a6da281353d083c024cca8d73d15350ef76d2a
Floogle <mrflooglebinder@gmail.com>
2024-04-25 14:59:00 +0800

    ES6 Classes - ES6 Classes

--- Part-2-AdvancedTopics/4-es6Classes/1-es6Classes.js
+++ Part-2-AdvancedTopics/4-es6Classes/1-es6Classes.js
@@ -13,14 +13,23 @@
 // Note: in ES6, Classes are just syntactic sugar over the existing prototype-based inheritance and they are at their core just functions. 
 
 class Circle {
+    // define the constructor function
     constructor(radius) {
         this.radius = radius;
+
+        // This will be added to the object.
         this.move = function() {}
     }
 
+    // Where as this will be added to the prototype of the object.
     draw() {
         console.log('draw');
     }
 }
 
 const c = new Circle(1);
+
+// print the type of circle to the console.
+// As you can see the type of Circle is a function it is not really a class.
+console.log(typeof Circle); // function
+console.log(c.constructor); // [Function: Circle]
